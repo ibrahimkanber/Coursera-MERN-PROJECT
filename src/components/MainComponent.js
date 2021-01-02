26da95ff106a659aa157b756f96802bd21dd4062
ibrahimKanber <ikanber45@gmail.com>
2021-01-02 14:07:02 +0100

    ADD HEADER AND FOOTER

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,25 +1,24 @@
 import {useState} from "react"
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { MenuComponent } from './MenuComponent';
 import { DishdetailComponent } from './DishdetailComponent';
 import {DISHES} from "../shared/dishes"
+import { HeaderComponent } from "./HeaderComponent";
+import { FooterComponent } from "./FooterComponent";
 
 function Main() {
-    const [dishes,setDishes]=useState(DISHES)
+  
+  const [dishes,setDishes]=useState(DISHES)
   const [selectedDishId,setSelectedDishId]=useState("")
 
   const onDishSelect=(dishId)=>{
       setSelectedDishId(dishId)
   }
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+    <div>
+      <HeaderComponent/>
       <MenuComponent dishes={DISHES} onDishSelect={onDishSelect}/>
       <DishdetailComponent dish={dishes.filter(dish=>dish.id===selectedDishId)[0]}/>
+      <FooterComponent/>
     </div>
   );
 }
